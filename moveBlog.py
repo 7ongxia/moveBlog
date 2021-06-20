722377c7f5ca82c6f8a4a3fc851eb5f1d6c0a675
7ongxia <7ongxia@gmail.com>
2021-06-20 17:54:51 +0800

    Retreive an egloos content as a HTML format.
    Retreive an egloos content as a HTML format.

--- moveBlog.py
+++ moveBlog.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 from datetime import datetime
 import time
 import webbrowser
@@ -53,9 +53,20 @@ class Tistory:
         }
         response = requests.post(url, data=data).json()
         print(json.dumps(response, indent=4, sort_keys=True))
+    
+    def post_image(self, image):
+        url = "https://www.tistory.com/apis/post/attach"
+        file = {'uploadedfile': image}
+        data = {
+            'access_token': f'{self.accessToken}',
+            'output': f'{self.outputType}',
+            'blogName': f'{self.blogName}'
+        }
+        response = requests.post(url, data=data, files=file).json()
+        print(json.dumps(response, indent=4, sort_keys=True))
 
 
-def getEgloosPost(i):
+def getEgloosPost(i, tistory):
     req = requests.get(f"http://SECRET/page/{i}")
     soup = BeautifulSoup(req.text, 'html.parser')
 
@@ -64,11 +75,17 @@ def getEgloosPost(i):
     # timestamp = time.mktime(datetime.strptime(s, "%Y/%m/%d %H:%M").timetuple())
     # print(timestamp)
 
-    # 본문
-    body = soup.select('.hentry')[0].get_text('\n\n')[:-20]
-    # print(body, '\n')
+    # 본문 (HTML)
+    content = soup.find_all("div", {'class':'hentry'})[0]
+    for div in content.find_all("span", {'class':'copyright_entry'}): 
+        div.decompose()
+    for div in content.find_all('div', attrs={'style': 'line-height:12px;padding:35px 0 5px'}):
+        div.decompose()
+    for comments in content.findAll(text=lambda text:isinstance(text, Comment)):
+        comments.extract()
 
     # 제목
+    body = soup.select('.hentry')[0].get_text('\n\n')[:-20]
     title = soup.select('.entry-title a')[0].text
     if title[:2] == '20':
         title = body.split('\n', 1)[0]
@@ -85,7 +102,8 @@ def getEgloosPost(i):
     return {
         'title': title,
         'body': body,
-        'image': images
+        'image': images,
+        'content': content
     }
 
 
@@ -95,8 +113,12 @@ if __name__ == "__main__":
     for i in range(1, 151):
         post = getEgloosPost(i)
         time.sleep(1)
-        tistory.post_write(title=post['title'], content=post['body'])
+        tistory.post_write(title=post['title'], content=post['content'])
         time.sleep(1)
-        break
 
-# TODO: Retreive an egloos content as a HTML format. + Image uploading function
+    # post = getEgloosPost(35, tistory)
+    # tistory.post_write(title=post['title'], content=post['content'])
+    # print(post['content'])
+
+
+# TODO: Image uploading function
